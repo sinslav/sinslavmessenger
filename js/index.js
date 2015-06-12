00650786d7a20b64f3f4abed80c6be63847dab2e
sinslav <sinslav@gmail.com>
2015-06-12 15:35:24 +0300

    add some comments

--- js/index.js
+++ js/index.js
@@ -17,12 +17,13 @@ function insertTemplates() {
 
     routie( {
             'dialog/:dial_id': function(dial_id){
+                console.log(dial_id); //Почему не возвращается айди?
                 var crrntData = {};
                 templator.centerColumn.innerHTML = '';
                 crrntData.messages = _.where(messagesObject, {'dialog_id': dial_id});
                 crrntData.users = usersObject;
 
-                console.log(crrntData.messages);
+                console.log(crrntData.messages);// На выходе получается пустой массив... а должен быть из тех, у которых соответствует айди диалогов
                 templator.centerColumn.innerHTML = (Mustache.render(templator.centerColumnTemplate, crrntData));
 
             }
